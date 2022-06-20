0d4d962f579a38b26feeecff1f5203b41f533462
divsigma <pontsh@163.com>
2022-06-20 03:23:57 +0000

    feat: enable IPC using pipe and epoll

--- examples/multitasking/processpool.cc
+++ examples/multitasking/processpool.cc
@@ -2,7 +2,9 @@
 #include <assert.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <sys/epoll.h>
 #include <sys/types.h>
+#include <errno.h>
 
 // class: sub process
 class process {
@@ -33,14 +35,21 @@ public:
     }
 
     void run();
-    void run_parent();
-    void run_child();
 
 private:
     processpool(int listenfd, int num_process);
 
+    void setup_epoll();
+    void epoll_addfd(int epollfd, int fd);
+
+    void run_parent();
+    void run_child();
+
 private:
     int m_listenfd;
+
+    int m_epollfd;
+
     int m_num_process;
     int m_sub_idx;
     process* m_sub_process;
@@ -48,6 +57,7 @@ private:
     static processpool<T>* m_instance;
 
     static const int k_max_num_process = 10;
+    static const int k_max_num_epoll_event = 1000;
 
 };
 
@@ -99,18 +109,77 @@ void processpool<T>::run() {
 }
 
 template<typename T>
+void processpool<T>::setup_epoll() {
+    m_epollfd = epoll_create(5);
+    assert(m_epollfd != -1);
+}
+
+static int setnonblocking(int fd) {
+    return 0;
+}
+
+template<typename T>
+void processpool<T>::epoll_addfd(int epollfd, int fd) {
+
+    epoll_event ev;
+    ev.events = EPOLLIN | EPOLLET;
+    ev.data.fd = fd;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &ev);
+
+}
+
+template<typename T>
 void processpool<T>::run_parent() {
     while (1) {
-        sleep(2);
-        printf("[parent]\n");
+        sleep(1);
+        for (int i = 0; i < m_num_process; i++) {
+            char msg[10] = "<- parent";
+            send(m_sub_process[i].m_pipefd[1], msg, sizeof(msg), 0);
+            printf("[parent] sent to %d\n", i);
+        }
+//        sleep(2);
+//        printf("[parent]\n");
     }
 }
 
 template<typename T>
 void processpool<T>::run_child() {
+
+    setup_epoll();
+
+    int paproc_pipefd = m_sub_process[m_sub_idx].m_pipefd[0];
+    epoll_addfd(m_epollfd, paproc_pipefd);
+
+    epoll_event ev[k_max_num_epoll_event];
+    int num = 0;
+    int ret = -1;
+
     while (1) {
-        sleep(1);
-        printf("[%d]\n", m_sub_idx);
+        num = epoll_wait(m_epollfd, ev, k_max_num_epoll_event, -1);
+        if (num < 0 && errno != EINTR) {
+            printf("[ERROR] epoll failure\n");
+            break;
+        }
+
+        for (int i = 0; i < num; i++) {
+            int ev_fd = ev[i].data.fd;
+            uint32_t ev_event = ev[i].events;
+            if (ev_fd == paproc_pipefd && (ev_event & EPOLLIN)) {
+                char msg[10];
+                ret = recv(ev_fd, msg, sizeof(msg), 0);
+                if ((ret < 0 && errno != EAGAIN) || !ret) {
+                    // non-blocking way
+                    printf("[proc-%d] *\n", m_sub_idx);
+                    continue;
+                } else {
+                    printf("[proc-%d] got msg: %s\n", m_sub_idx, msg);
+                }
+
+            }
+        }
+
+//        sleep(1);
+//        printf("[%d]\n", m_sub_idx);
     }
 }
 
